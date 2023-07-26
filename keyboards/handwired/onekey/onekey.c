123c3d014ecdb0d3ab17226ae4a923125d0855d3
Kweku Boakye <kwe.boakye@googlemail.com>
2023-07-26 02:28:55 +0100

    configured rgb_matrix for fractyl addd onekey keymap to test IS31FL3743A breakout board

--- keyboards/handwired/onekey/onekey.c
+++ keyboards/handwired/onekey/onekey.c
@@ -10,3 +10,26 @@ void keyboard_post_init_kb(void) {
     debug_mouse    = true;
     keyboard_post_init_user();
 }
+
+#if RGB_MATRIX_ENABLE
+const is31_led __flash g_is31_leds[RGB_MATRIX_LED_COUNT] = {
+/* Refer to IS31 manual for these locations
+ *   driver
+ *   |  R location
+ *   |  |        G location
+ *   |  |        |        B location
+ *   |  |        |        | */
+    {0, CS18_SW1, CS17_SW1, CS16_SW1},
+};
+
+led_config_t g_led_config = { {
+  // Key Matrix to LED Index
+ {0}
+}, {
+  // LED Index to Physical Position
+  {112, 32}
+}, {
+  // LED Index to Flag
+  4
+} };
+#endif
