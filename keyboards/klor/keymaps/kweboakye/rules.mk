84aaeb3e93a3da48a67ac83be08ec34d71a194f8
Kweku Boakye <kwe.boakye@googlemail.com>
2024-05-13 21:38:04 +0100

    klor changes

--- keyboards/klor/keymaps/kweboakye/rules.mk
+++ keyboards/klor/keymaps/kweboakye/rules.mk
@@ -7,6 +7,9 @@ BOOTLOADER = rp2040
 # LTO must be disabled for RP2040 builds
 LTO_ENABLE = no
 
+# Ignore some warnings during the build, likely to be fixed before RP2040 PR is merged
+ALLOW_WARNINGS = yes
+
 SPLIT_KEYBOARD = yes
 SWAP_HANDS_ENABLE = yes
 
@@ -23,7 +26,7 @@ HAPTIC_DRIVER = drv2605l
 
 
 #PER KEY RGB
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes      # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
 RGB_MATRIX_DRIVER = ws2812  # RGB matrix driver support
@@ -37,6 +40,19 @@ QUANTUM_PAINTER_ENABLE = yes
 QUANTUM_PAINTER_DRIVERS += st7789_spi
 ENCODER_ENABLE = no
 
-PIN_COMPATIBLE = elite_c
-CONVERT_TO=rp2040_ce
+#PIN_COMPATIBLE = elite_c
+#CONVERT_TO=elite_pi
 BACKLIGHT_ENABLE = yes
+
+OS_DETECTION_ENABLE = yes
+UNICODEMAP_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = azoteq_iqs5xx
+WPM_ENABLE = yes
+QUANTUM_PAINTER_ENABLE = yes
+MAGIC_ENABLE = yes
+QUANTUM_PAINTER_DRIVERS += st7789_spi
+QUANTUM_PAINTER_LVGL_INTEGRATION = yes
+BACKLIGHT_DRIVER = pwm
+CONSOLE_ENABLE = yes
+#BOOTMAGIC_ENABLE = yes
